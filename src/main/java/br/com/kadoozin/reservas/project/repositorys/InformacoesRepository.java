88234697d4a7199e065a46c9370b1e36bb117aaf
Kaio Cesar <kadoozin@gmail.com>
2025-10-18 08:54:42 -0300

    feat(repository): criar repositórios de informacoes, endereco e telefone

---
+++ src/main/java/br/com/kadoozin/reservas/project/repositorys/InformacoesRepository.java
@@ -0,0 +1,9 @@
+package br.com.kadoozin.reservas.project.repositorys;
+
+import br.com.kadoozin.reservas.project.entities.Informacoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InformacoesRepository extends JpaRepository<Informacoes, Long> {
+}
